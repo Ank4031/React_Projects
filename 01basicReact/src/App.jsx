e6c58e51651090b04d053c98902c70ec03632c8d
Ank4031 <bluepearl4031@gmail.com>
2025-08-13 14:37:08 +0530

    first commit

---
+++ 01basicReact/src/App.jsx
@@ -0,0 +1,12 @@
+import Work from "./test1.jsx"
+
+function App() {
+  return (
+    <>
+    <Work/>
+    <p>This part consist of the working of files in react,<br/> understand the flow and structure and understand what is fragment.</p>
+    </>
+  );
+}
+
+export default App
---
+++ 01basicReact/src/test1.jsx
@@ -0,0 +1,7 @@
+function Work(){
+    return(
+        <h1>Ankit is working on basics part 1.</h1>
+    )
+}
+
+export default Work
